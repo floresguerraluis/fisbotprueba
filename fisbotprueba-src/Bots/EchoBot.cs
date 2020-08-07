1652eff41ff318d48538e9773b6267ce9ab5673a
floresguerraluis <69281948+floresguerraluis@users.noreply.github.com>
2020-08-07 09:49:11 -0500

    nuevo 6
    nuevo 7

--- fisbotprueba-src/Bots/EchoBot.cs
+++ fisbotprueba-src/Bots/EchoBot.cs
@@ -19,7 +19,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "funciona123";
+            var welcomeText = "hola amigo";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
